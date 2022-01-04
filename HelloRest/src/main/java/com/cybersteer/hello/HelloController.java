260e2ed6a447fb059d9a22190395d6b77fdf4588
name <cybersteer@naver.com>
2022-01-04 23:28:59 +0900

    hello

--- HelloRest/src/main/java/com/cybersteer/hello/HelloController.java
+++ HelloRest/src/main/java/com/cybersteer/hello/HelloController.java
@@ -1,14 +1,22 @@
 package com.cybersteer.hello;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-@Controller
+@RestController
+//@Controller
 public class HelloController {
 	@GetMapping("/hello")
+//	public String hi(Model model) {
+//        Random random = new Random();
+//        model.addAttribute("myValue", "안녕! 이건 스프링 부트입니다! " + random.nextInt());
+//		return "hello";
+//	}
 	public String hi() {
-		return "hello.html";
+		return "<h1>test</h1>";
 	}
 }
