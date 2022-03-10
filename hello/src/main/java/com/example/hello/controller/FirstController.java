c8353299071223f202d77f659f02c0b19ebd7c8a
name <cybersteer@naver.com>
2022-03-11 00:13:59 +0900

    layout

--- hello/src/main/java/com/example/hello/controller/FirstController.java
+++ hello/src/main/java/com/example/hello/controller/FirstController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String helloCOmponent(Model model){
+    public String helloComponent(Model model){
         model.addAttribute("name","component");
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
+    }
+
+    @GetMapping("/bye")
+    public  String seeYouNext(Model model){
+        model.addAttribute("name", "component");
+        return "goodbye";
     }
 }
