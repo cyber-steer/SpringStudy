c8d469f7ad4046f167a3cd66fa76b1a922892d3a
name <cybersteer@naver.com>
2022-03-12 09:01:48 +0900

    form data input to dto

---
+++ hello/src/main/java/com/example/hello/controller/ArticleController.java
@@ -0,0 +1,21 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
+}
