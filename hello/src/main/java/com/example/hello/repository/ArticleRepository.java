b570a862abffd880e3a443dcdce0071d44b63eaa
name <cybersteer@naver.com>
2022-07-01 23:28:36 +0900

    reset

--- hello/src/main/java/com/example/hello/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hello.repository;
-
-import com.example.hello.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-
-}
